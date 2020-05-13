8912bb88a81cd1d586a054877048a3726b1153de
Albion <albionx@gmail.com>
2020-05-12 22:43:03 -0400

    Updating script
    > Better support for null fields
    > Support for timestamp type fields
    > More elegant KeyboardInterrupt handling
    > Support for 3 more datasets

--- downloader.py
+++ downloader.py
@@ -3,18 +3,12 @@
 
 import json
 import sqlite3
-import time
 import urllib.request
 
-BASE_URL = 'https://datos.hcdn.gob.ar:443/'
-SOURCE_NAME = 'Leyes'
-DATABASE_NAME = 'congreso.sqlite'
-
-def runDataImporter():
+def runDataImporter(resourcePathCursor):
     queryTally = int()
 
     # This path provides initial access, the API will give a cursor to the next results
-    resourcePathCursor = 'api/3/action/datastore_search?resource_id=a88b42c3-d375-4072-8542-92b11db1d711'
     resourceUrl = '{}{}'.format(BASE_URL, resourcePathCursor)
     resourceData = getJsonContents(resourceUrl)
 
@@ -63,25 +57,34 @@ def writeToDB(resourceData):
     sql = 'CREATE TABLE IF NOT EXISTS {} ({})'.format(SOURCE_NAME, fullSchema)
     cursor.execute(sql)
 
-    # Loop through records to insert into the database. Keep track of skips and additions.
+    # Loop through records to insert into the database. Keep track of additions.
     queryInsertions = int()
 
     for record in resourceData['result']['records']:
+
         # helper values
         fields = (str(list(record.keys()))[1:-1])
-        values = (str(list(record.values()))[1:-1])
+        # build the equivalent to 'fields' but changing the None values for "", so that i can use it in the SELECT query
+        values = list()
+        for value in record.values():
+            if value is None: 
+                values.append("")
+            else:
+                values.append(value)
+        values = str(values)[1:-1]
         conditions = buildQueryCondition(record, fieldTypes)
 
         # See if the record is present, and if so, skip
-        sql = 'SELECT ROWID FROM {} WHERE {}'.format(SOURCE_NAME, conditions)
+        # TODO: find out how to make detect changes and do updates. Maybe if the primary key is the same but the contents change?
+        select_sql = 'SELECT ROWID FROM {} WHERE {}'.format(SOURCE_NAME, conditions)
         try:
-            cursor.execute(sql)
+            cursor.execute(select_sql)
         except:
             print ('Problem running query:', sql)
 
         if cursor.fetchone() is None:
-            sql = 'INSERT INTO {} ({}) VALUES ({})'.format(SOURCE_NAME, fields, values)
-            cursor.execute(sql)
+            insert_sql = 'INSERT INTO {} ({}) VALUES ({})'.format(SOURCE_NAME, fields, values)
+            cursor.execute(insert_sql)
             queryInsertions += 1
 
     database.commit()
@@ -105,14 +108,56 @@ def getJsonContents(url):
 
 def buildQueryCondition(record, fieldTypes):
     conditions = str()
-    for fieldName in record.keys():
-        if fieldTypes[fieldName].lower() == 'text':
-            # avoid SLQ injection by preventing closing of the double quotes
-            conditions = '{} AND {} = "{}"'.format(conditions, fieldName, record[fieldName].replace('\"','\''))
-        else:
-            # avoid SLQ injection by preventing a space
-            conditions = '{} AND {} = {}'.format(conditions, fieldName, str(record[fieldName]).replace(' ',''))
-    return conditions[5:]
+    try:
+        for fieldName in record.keys():
+            # Deal with None values by using "". In text fields this becomes a "" filed. In numeric fields it becomes NULL.
+            if record[fieldName] == None: 
+                conditions = '{} AND {} = ""'.format(conditions, fieldName)
+                continue
+            # Deal with formatting and SQL injection protection.
+            if fieldTypes[fieldName].lower() == 'text' or fieldTypes[fieldName].lower() == 'timestamp':
+                conditions = '{} AND {} = "{}"'.format(conditions, fieldName, record[fieldName].replace('\"','\''))
+            else:
+                conditions = '{} AND {} = {}'.format(conditions, fieldName, str(record[fieldName]).replace(' ',''))
+        return conditions[5:]
+    except:
+        print (record)
+        print ((str(list(record.keys()))[1:-1]))
+        print ((str(list(record.values()))[1:-1]))
+        quit()
 
 if __name__ == '__main__':
-    runDataImporter()
+
+    DATABASE_NAME = 'congreso.sqlite'
+    BASE_URL = 'https://datos.hcdn.gob.ar:443'
+
+    decision = input(
+"""What dataset do you want to download:\n
+1. Subsidios
+2. Leyes
+3. Sesiones
+4. Diputados
+""")
+    
+    if decision.lower() == '1':
+        SOURCE_NAME = 'Subsidios'
+        resourcePathCursor = '/api/3/action/datastore_search?resource_id=2cdaef71-f802-4067-bfa0-810dd3a22583'
+    elif decision.lower() == '2':
+        SOURCE_NAME = 'Leyes'
+        resourcePathCursor = '/api/3/action/datastore_search?resource_id=a88b42c3-d375-4072-8542-92b11db1d711'
+    elif decision.lower() == '3':
+        SOURCE_NAME = 'Sesiones'
+        resourcePathCursor = '/api/3/action/datastore_search?resource_id=4ac70a51-a82d-428b-966a-0a203dd0a7e3'
+    elif decision.lower() == '4':
+        SOURCE_NAME = 'Diputados'
+        resourcePathCursor = '/api/3/action/datastore_search?resource_id=16cd699d-83fb-4d5f-afd4-0af9b47b1bd7'
+    else:
+        quit()
+    
+    try:
+        runDataImporter(resourcePathCursor)
+    except KeyboardInterrupt:
+        print ('User quit with an interrupt')
+        quit()
+    except:
+        print ('Some uncaught happened...')
