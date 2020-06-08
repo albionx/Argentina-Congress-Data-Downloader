acfc4c82be6f8823d464fe5ef09e2ab80bad81b2
albionx <albionx@gmail.com>
2020-06-07 22:23:55 -0400

    Update downloader.py
    Addresses Issue #4

--- downloader.py
+++ downloader.py
@@ -21,52 +21,45 @@ import sqlite3
 import urllib.request
 from time import sleep
 
-def runDataImporter():
-    global resourcePathCursor # needed because I'm reassigning this variable in this function.
+def runDataImporter(resourcePathCursor):
 
-    queryTally = int()
+    MoreData = True
 
-    # This path provides initial access, the API will give a cursor to the next results
-    resourceUrl = '{}{}'.format(BASE_URL, resourcePathCursor)
-    resourceData = getJsonContents(resourceUrl)
+    while MoreData:
+         # This path provides initial access, the API will give a cursor to the next results
+        resourceUrl = '{base_url}{path}'.format(base_url=BASE_URL, path=resourcePathCursor)
+        resourceData = getJsonContents(resourceUrl)
 
-    while True:
         # Catch failures on their backend
         if resourceData.get('success') != True: 
             print ('Query didn\'t execute successfully:', resourceData['success'])
             break
 
         # Count total number of records in dataset and in this particular resultset
+        queryTally = int()
         queryResults = len(resourceData['result']['records'])
         queryTotal = resourceData['result']['total']
-        queryTally = queryTally + queryResults
+        queryTally += queryResults
 
         queryInsertions = writeToDB(resourceData)
 
         # Establish progress in an unnecessarily visual way
-        progress = (queryTally / queryTotal)
-        print('Downloading {} data: {}{} {}% ({}/{}) \r'.format(
-            SOURCE_NAME,
-            "█" * round(progress * 20),
-            "░" * (20 - round(progress * 20)),
-            round(progress * 100),
-            queryTally,
-            queryTotal
+        print('Downloading {table} data: {progress}{remnant} {percentage}% ({sofar}/{total}) \r'.format(
+            table=SOURCE_NAME,
+            progress="█" * round((queryTally / queryTotal) * 20),
+            remnant="░" * (20 - round((queryTally / queryTotal) * 20)),
+            percentage=round((queryTally / queryTotal) * 100),
+            sofar=queryTally,
+            total=queryTotal
             ), end="")
-        #print ('Found {} results in this page: inserted {} records and skipped {}.'.format(queryResults, queryInsertions, queryResults - queryInsertions))
-        #print ('We are {}% of the way there'.format(round((queryTally / queryTotal) * 100)))
-        #print ('=' * 30)
+
+        # update the cursor
+        resourcePathCursor = resourceData['result']['_links']['next']
 
         # Exit if we've reached 100% already
         if queryTally == queryTotal: 
-            print ('\nSUCCESS! Data saved in the \'{}\' table, in the \'{}\' file.'.format(SOURCE_NAME, DATABASE_NAME))
-            break
-
-        # Look for the next results
-        if queryResults > 0: # there is potentially more to be obtained
-            resourcePathCursor = resourceData['result']['_links']['next']
-            resourceUrl = '{}{}'.format(BASE_URL, resourcePathCursor)
-            resourceData = getJsonContents(resourceUrl)
+            print ("\nSUCCESS! Data saved in the '{table}' table, in the '{file}' file.".format(table=SOURCE_NAME, file=DATABASE_NAME))
+            MoreData = False
 
 def writeToDB(resourceData):
 
@@ -75,7 +68,7 @@ def writeToDB(resourceData):
     fieldTypes = dict() # this dictionary stores each field and the type, which we'll use when doing the WHERE clause
 
     for field in resourceData['result']['fields']:
-        fullSchema = '{}, {} {}'.format(fullSchema, field['id'], field['type'])
+        fullSchema = '{fullSchema}, {id} {type}'.format(fullSchema=fullSchema, id=field['id'], type=field['type'])
         fieldTypes[field['id']] = field['type']
 
     fullSchema = fullSchema[1:]
@@ -83,7 +76,7 @@ def writeToDB(resourceData):
      # Create table with the fullSchema collected before
     database = sqlite3.connect(DATABASE_NAME)
     cursor = database.cursor()
-    sql = 'CREATE TABLE IF NOT EXISTS {} ({})'.format(SOURCE_NAME, fullSchema)
+    sql = 'CREATE TABLE IF NOT EXISTS {table} ({schema})'.format(table=SOURCE_NAME, schema=fullSchema)
     cursor.execute(sql)
 
     # Loop through records to insert into the database. Keep track of additions.
@@ -93,6 +86,7 @@ def writeToDB(resourceData):
 
         # helper values
         fields = (str(list(record.keys()))[1:-1])
+
         # build the equivalent to 'fields' but changing the None values for "", so that i can use it in the SELECT query
         values = list()
         for value in record.values():
@@ -105,14 +99,14 @@ def writeToDB(resourceData):
 
         # See if the record is present, and if so, skip
         # TODO: Allow for records to be updated, by checking only for an ID and then verifying if the contents are different before doing an update
-        select_sql = 'SELECT ROWID FROM {} WHERE {}'.format(SOURCE_NAME, conditions)
+        select_sql = 'SELECT ROWID FROM {table} WHERE {conditions}'.format(table=SOURCE_NAME, conditions=conditions)
         try:
             cursor.execute(select_sql)
         except:
             print ('Problem running query:', sql)
 
         if cursor.fetchone() is None:
-            insert_sql = 'INSERT INTO {} ({}) VALUES ({})'.format(SOURCE_NAME, fields, values)
+            insert_sql = 'INSERT INTO {table} ({fields}) VALUES ({values})'.format(table=SOURCE_NAME, fields=fields, values=values)
             cursor.execute(insert_sql)
             queryInsertions += 1
 
@@ -124,15 +118,15 @@ def getJsonContents(url):
     data = None
     try:
         sleep(DELAY_IN_SECONDS)
-        urlHandler = urllib.request.urlopen(url)
-        urlContents = urlHandler.read().decode()
-        response_code = urlHandler.getcode()
+        with urllib.request.urlopen(url) as urlHandler:
+            urlContents = urlHandler.read().decode()
+            response_code = urlHandler.getcode()
         if response_code == 200:
             data = json.loads(urlContents)
         else:
-            print('Error retrieving contents from URL {}'.format(url))
+            print('Error retrieving contents from URL {url}'.format(url=url))
     except Exception as ex:
-        print ('Problem obtaining or parsing the data from: {} - Error {}'.format(url, ex))
+        print ('Problem obtaining or parsing the data from: {url} - Error {ex}'.format(url=url, ex=ex))
     return data
 
 def buildQueryCondition(record, fieldTypes):
@@ -141,13 +135,13 @@ def buildQueryCondition(record, fieldTypes):
         for fieldName in record.keys():
             # Deal with None values by using "". In text fields this becomes a "" filed. In numeric fields it becomes NULL.
             if record[fieldName] == None: 
-                conditions = '{} AND {} = ""'.format(conditions, fieldName)
+                conditions = '{conditions} AND {field} = ""'.format(conditions=conditions, field=fieldName)
                 continue
             # Deal with formatting and SQL injection protection.
             if fieldTypes[fieldName].lower() == 'text' or fieldTypes[fieldName].lower() == 'timestamp':
-                conditions = '{} AND {} = "{}"'.format(conditions, fieldName, record[fieldName].replace('\"','\''))
+                conditions = '{conditions} AND {field} = "{value}"'.format(conditions=conditions, field=fieldName, value=record[fieldName].replace('\"','\''))
             else:
-                conditions = '{} AND {} = {}'.format(conditions, fieldName, str(record[fieldName]).replace(' ',''))
+                conditions = '{conditions} AND {field} = {value}'.format(conditions=conditions, field=fieldName, value=str(record[fieldName]).replace(' ',''))
         return conditions[5:]
     except:
         print (record)
@@ -176,20 +170,20 @@ Which dataset do you want to download:
 """)
         if decision.lower() == '1':
             SOURCE_NAME = 'Subsidies'
-            resourcePathCursor = '/api/3/action/datastore_search?resource_id=2cdaef71-f802-4067-bfa0-810dd3a22583'
+            resourcePath = '/api/3/action/datastore_search?resource_id=2cdaef71-f802-4067-bfa0-810dd3a22583'
         elif decision.lower() == '2':
             SOURCE_NAME = 'Laws'
-            resourcePathCursor = '/api/3/action/datastore_search?resource_id=a88b42c3-d375-4072-8542-92b11db1d711'
+            resourcePath = '/api/3/action/datastore_search?resource_id=a88b42c3-d375-4072-8542-92b11db1d711'
         elif decision.lower() == '3':
             SOURCE_NAME = 'Sessions'
-            resourcePathCursor = '/api/3/action/datastore_search?resource_id=4ac70a51-a82d-428b-966a-0a203dd0a7e3'
+            resourcePath = '/api/3/action/datastore_search?resource_id=4ac70a51-a82d-428b-966a-0a203dd0a7e3'
         elif decision.lower() == '4':
             SOURCE_NAME = 'Representatives'
-            resourcePathCursor = '/api/3/action/datastore_search?resource_id=16cd699d-83fb-4d5f-afd4-0af9b47b1bd7'
+            resourcePath = '/api/3/action/datastore_search?resource_id=16cd699d-83fb-4d5f-afd4-0af9b47b1bd7'
         elif decision.lower() == '5' or decision.lower() == '':
             quit()
 
-        runDataImporter()
+        runDataImporter(resourcePath)
 
     except KeyboardInterrupt:
         print ('\nUser quit with an interrupt')
